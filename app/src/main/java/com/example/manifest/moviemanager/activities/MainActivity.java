a921155035d21457120fb19efe93476a5395c996
tv.unimol <doslovno@gmail.com>
2018-09-14 21:32:11 +0300

    Add activities and fragments packages and move MainActivity to activities

--- app/src/main/java/com/example/manifest/moviemanager/activities/MainActivity.java
+++ app/src/main/java/com/example/manifest/moviemanager/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.manifest.moviemanager;
+package com.example.manifest.moviemanager.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.manifest.moviemanager.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
