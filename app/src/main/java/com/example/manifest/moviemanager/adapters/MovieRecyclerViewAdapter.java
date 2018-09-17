71f8168637dce09d8303e3171bd9938a0fc0cd16
tv.unimol <doslovno@gmail.com>
2018-09-17 18:22:06 +0300

    Click on any movie now starts MovieDetailActivity

--- app/src/main/java/com/example/manifest/moviemanager/adapters/MovieRecyclerViewAdapter.java
+++ app/src/main/java/com/example/manifest/moviemanager/adapters/MovieRecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.manifest.moviemanager.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.manifest.moviemanager.R;
+import com.example.manifest.moviemanager.activities.MovieDetailActivity;
 import com.example.manifest.moviemanager.models.Movie;
 import com.squareup.picasso.Picasso;
 
@@ -48,7 +50,7 @@ public class MovieRecyclerViewAdapter extends RecyclerView.Adapter<MovieRecycler
         return movies.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         @BindView(R.id.ivMovieImage)
         ImageView ivMovieImage;
         @BindView(R.id.tvTitle)
@@ -61,6 +63,16 @@ public class MovieRecyclerViewAdapter extends RecyclerView.Adapter<MovieRecycler
         ViewHolder(View view) {
             super(view);
             ButterKnife.bind(this, view);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Movie movie = movies.get(getAdapterPosition());
+            Intent intent = new Intent(context, MovieDetailActivity.class);
+            intent.putExtra("MOVIE", movie);
+            context.startActivity(intent);
+
         }
     }
 }
